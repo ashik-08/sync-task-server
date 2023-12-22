bbd1fe82e07efeae18e6747e82dfa37d3aeac549
ashik-08 <ashikurrahman.8october@gmail.com>
2023-12-22 08:14:05 +0600

    added /users route and POST method

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const port = process.env.PORT || 5005;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // connect to the database & access it's collections
+    const database = client.db("sync-task");
+    const usersCollection = database.collection("users");
+
+    // user related API (usersCollection)
+    // add new user credentials to the db
+    app.post("/users", async (req, res) => {
+      try {
+        const user = req.body;
+        // query to find all users in the collection
+        const query = await usersCollection.find().toArray();
+        // check if there already exist an user
+        const found = query.find(
+          (search) => search.name === user.name && search.email === user.email
+        );
+        if (found) {
+          return res.send({ message: "Already exists" });
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+        return res.send({ error: true, message: error.message });
+      }
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("SyncTask server is running!");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}`);
+});
